ec8d3ab61c9b2bacb0095ceab9086562e2b7f978
breemark <britomaximiliano83@gmail.com>
2023-08-06 11:21:30 +0700

    12. Testing Livewire Blades

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -1,15 +1,15 @@
 <div>
     <h1>{{ __('Create Article') }}</h1>
-    <form wire:submit.prevent='save' >
+    <form wire:submit.prevent="save" >
         @csrf
         <label>
-            <input wire:model='article.title' type="text" placeholder="{{ __('Title') }}" >
+            <input wire:model="article.title" type="text" placeholder="{{ __('Title') }}" >
             @error('article.title')
                 <div>{{ $message }}</div>
             @enderror
         </label>
         <label>
-            <textarea wire:model='article.content' placeholder="{{ __('Content') }}"></textarea>
+            <textarea wire:model="article.content" placeholder="{{ __('Content') }}"></textarea>
             @error('article.content')
                 <div>{{ $message }}</div>
             @enderror
--- tests/Feature/Livewire/ArticleFormTest.php
+++ tests/Feature/Livewire/ArticleFormTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Livewire;
 
+use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -13,8 +14,27 @@ class ArticleFormTest extends TestCase
 
     use RefreshDatabase;
 
-    /** @test */
-    function can_create_new_articles()
+    function test_article_form_renders()
+    {
+        $this->get(route('articles.create'))
+            ->assertSeeLivewire('article-form');
+
+        $article = Article::factory()->create();
+
+        $this->get(route('articles.edit', $article))
+            ->assertSeeLivewire('article-form');
+    }
+
+    function test_blade_template_is_wired()
+    {
+        Livewire::test('article-form')
+            ->assertSeeHtml('wire:submit.prevent="save"')
+            ->assertSeeHtml('wire:model="article.title"')
+            ->assertSeeHtml('wire:model="article.content"')
+        ;
+    }
+
+    function test_can_create_new_articles()
     {
         Livewire::test('article-form')
             ->set('article.title', 'New Article')
@@ -30,6 +50,24 @@ class ArticleFormTest extends TestCase
         ]);
     }
 
+    function test_can_update_articles()
+    {
+        $article = Article::factory()->create();
+        Livewire::test('article-form', ['article' => $article])
+            ->assertSet('article.title', $article->title)
+            ->assertSet('article.content', $article->content)
+            ->set('article.title', 'New Title 0608')
+            ->call('save')
+            ->assertSessionHas('status')
+            ->assertRedirect(route('home'))
+        ;
+
+        $this->assertDatabaseHas('articles', [
+            'title' => 'New Title 0608'
+        ]);
+
+        $this->assertDatabaseCount('articles', 1);
+    }
 
     function test_title_is_required()
     {
@@ -37,9 +75,10 @@ class ArticleFormTest extends TestCase
             ->set('article.content', 'Article Content')
             ->call('save')
             ->assertHasErrors(['article.title' => 'required'])
+            // Check the Blade component too
+            ->assertSeeHtml(__('validation.required', ['attribute' => 'title']))
         ;
     }
-
 
     function test_title_must_be_4_characters_min()
     {
@@ -47,6 +86,11 @@ class ArticleFormTest extends TestCase
             ->set('article.title', 'one')
             ->call('save')
             ->assertHasErrors(['article.title' => 'min:4'])
+            ->assertSeeHtml(__('validation.min.string', [
+                'attribute' => 'title',
+                'min' => 4
+            ]))
+
         ;
     }
 
@@ -56,6 +100,7 @@ class ArticleFormTest extends TestCase
             ->set('article.content', '')
             ->call('save')
             ->assertHasErrors(['article.content' => 'required'])
+            ->assertSeeHtml(__('validation.required', ['attribute' => 'content']))
         ;
     }
 
@@ -71,7 +116,7 @@ class ArticleFormTest extends TestCase
         ;
     }
 
-        function test_real_time_validation_works_for_content()
+    function test_real_time_validation_works_for_content()
     {
         Livewire::test('article-form')
             ->set('article.content', '')
