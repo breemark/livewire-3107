9f3436dcd28faf088b47f4cc2c7a0c5a123aa4a4
breemark <britomaximiliano83@gmail.com>
2023-08-08 15:45:26 +0700

    17. Styling with TailwindCSS

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -1,25 +1,45 @@
 <div>
-    <h1>{{ __('Create Article') }}</h1>
-    <form wire:submit.prevent="save" >
-        @csrf
-        <label>
-            <input wire:model="article.title" type="text" placeholder="{{ __('Title') }}" >
-            @error('article.title')
-                <div>{{ $message }}</div>
-            @enderror
-        </label>
-        <label>
-            <input wire:model="article.slug" type="text" placeholder="{{ __('URL') }}" >
-            @error('article.slug')
-                <div>{{ $message }}</div>
-            @enderror
-        </label>
-        <label>
-            <textarea wire:model="article.content" placeholder="{{ __('Content') }}"></textarea>
-            @error('article.content')
-                <div>{{ $message }}</div>
-            @enderror
-        </label>
-        <input type="submit" value="{{ __('Save') }}">
-    </form>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create Article') }}
+        </h2>
+    </x-slot>
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <x-form-section submit="save">
+                <x-slot name="title">
+                    {{ __('New Article') }}
+                </x-slot>
+                <x-slot name="description">
+                    {{ __('Some Description') }}
+                </x-slot>
+                <x-slot name="form">
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="title" :value="__('Title')"/>
+                        <x-input id="title" class="mt-1 block w-full" wire:model="article.title" type="text"/>
+                        <x-input-error class="mt-2" for="article.title" />
+                    </div>
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="slug" :value="__('Slug')"/>
+                        <x-input id="slug" class="mt-1 block w-full" wire:model="article.slug" type="text"/>
+                        <x-input-error class="mt-2" for="article.slug" />
+                    </div>
+
+                    <div class="col-span-6">
+                        <label>
+                            <textarea wire:model="article.content" placeholder="{{ __('Content') }}"></textarea>
+                            @error('article.content')
+                                <div>{{ $message }}</div>
+                            @enderror
+                        </label>
+                    </div>
+                    <x-slot name="actions">
+                        <x-button>
+                            {{ __('Save') }}
+                        </x-button>
+                    </x-slot>
+                </x-slot>
+            </x-form-section>
+        </div>
+    </div>
 </div>
