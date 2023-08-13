16706c3078cb6871d5a52f02f7048e373b8797dc
breemark <britomaximiliano83@gmail.com>
2023-08-13 18:10:52 +0700

    25. Form fixes

--- resources/views/components/html-editor.blade.php
+++ resources/views/components/html-editor.blade.php
@@ -1,7 +1,23 @@
 <div x-data="{ value: @entangle($attributes->wire('model')).defer }"
 	x-on:trix-change="value = $event.target.value"
+	@trix-file-accept.prevent
 >
-	<div wire:ignore>
-		<trix-editor :value="value" {!! $attributes->whereDoesntStartWith('wire:model')->merge(['class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) !!}></trix-editor>
+	<div wire:ignore >
+		<trix-editor 
+			:value="value" 
+			{!! 
+				$attributes->whereDoesntStartWith('wire:model')->merge([
+					'class' => 'border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm']) 
+			!!}>
+		</trix-editor>
 	</div>
 </div>
+
+
+
+<style>
+trix-toolbar [data-trix-button-group="file-tools"],
+trix-toolbar [data-trix-button-group="history-tools"] {
+    display: none;
+}
+</style>
